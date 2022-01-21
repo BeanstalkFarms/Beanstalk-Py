c2d3f9ab4db28239fea3dc2c891431ad50acd7be
funderberker <meellone01@gmail.com>
2022-01-21 14:21:30 -0600

    [monitoring] Only send stop msg in stage

--- python/bots/util.py
+++ python/bots/util.py
@@ -70,17 +70,17 @@ class Monitor():
         logging.info(f'Starting {self.name} monitoring thread...')
         if self._dry_run:
             self.message_function(f'{self.name} monitoring started (with simulated data).')
-        elif not self.prod:
-            self.message_function(f'{self.name} monitoring started.')
+        # elif not self.prod:
+        #     self.message_function(f'{self.name} monitoring started.')
         self._thread_active = True
         self._thread_wrapper.start()
 
     def stop(self):
-        if not self.prod:
-            logging.info(f'Stopping {self.name} monitoring thread...')
+        logging.info(f'Stopping {self.name} monitoring thread...')
+        # if not self.prod:
+            # self.message_function(f'{self.name} monitoring stopped.')
         self._thread_active = False
         self._thread_wrapper.join(3 * self.query_rate)
-        self.message_function(f'{self.name} monitoring stopped.')
 
     def _thread_wrapper_method(self):
         """
