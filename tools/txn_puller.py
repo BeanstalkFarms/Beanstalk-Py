e0be1fc6105009fbe2165c781e696d85b51c3c57
funderberker <meellone01@gmail.com>
2022-09-03 13:13:46 +0800

    [txn puller] cleaner print format, default to beanstalk abi, set up as module

--- tools/txn_puller.py
+++ tools/txn_puller.py
@@ -2,6 +2,7 @@
 Tool to extract all information about a transaction from chain.
 
 Example use:
+python3 -m tools.txn_puller 0x61ce4cfaf84ad70d83299c10e4dda09a6a593f8a328ac3db9e683088c2637f68
 python3 txn_puller.py 0x34f24706cb3cbd26da893e8abf181e1868c11f95a7f39891baa842f19e61b72f -p ../constants/abi/beanstalk_abi.json
 
 Web3 lib txn receipt structure:
@@ -25,12 +26,14 @@ TxReceipt = TypedDict("TxReceipt", {
 """
 
 import argparse
+from hexbytes.main import HexBytes
 import json
 import logging
 import os
 from pprint import pformat
 import time
 from web3 import Web3, WebsocketProvider
+from web3.datastructures import AttributeDict
 from web3.exceptions import TransactionNotFound
 from web3.logs import DISCARD
 
@@ -45,9 +48,10 @@ def decode_logs(txn_receipt, event):
 
 #  self.beanstalk_contract.events['PodOrderCancelled']().processReceipt(transaction_receipt, errors=eth_chain.DISCARD)
 
+
 def get_decoded_logs(txn_receipt, contract):
     """Get all decoded logs from a single contract in a receipt.
-    
+
     Returns:
         List of AttributeDicts containing individual decoded logs.
     """
@@ -56,17 +60,20 @@ def get_decoded_logs(txn_receipt, contract):
         decoded_logs.extend(decode_logs(txn_receipt, event()))
     return decoded_logs
 
+
 def get_event(contract, event_name):
     """Get a single Event object from a contract by name."""
     return contract.events[event_name]()
 
+
 def get_decoded_logs_by_event(txn_receipt, contract, event_name):
     """Get all decoded logs for a specific event in a single contract in a receipt.
-    
+
     Returns:
         List of AttributeDicts containing individual decoded logs.
     """
     return decode_logs(txn_receipt, get_event(contract, event_name))
+
 
 def get_txn_receipt_or_wait(web3, txn_hash, max_retries=5):
     """Get the transaction receipt and handle errors and block delays cleanly.
@@ -84,7 +91,8 @@ def get_txn_receipt_or_wait(web3, txn_hash, max_retries=5):
             return web3.eth.get_transaction_receipt(txn_hash)
         except TransactionNotFound as e:
             if try_count > max_retries:
-                logging.error(f'Failed to get txn after {try_count} retries. Was the block orphaned?')
+                logging.error(
+                    f'Failed to get txn after {try_count} retries. Was the block orphaned?')
                 raise(e)
             logging.warning(f'Failed to get txn receipt. Retrying...\n{e}')
         # ~ 1 ETH block time.
@@ -92,10 +100,31 @@ def get_txn_receipt_or_wait(web3, txn_hash, max_retries=5):
         try_count += 1
 
 
+def format_log_str(log, indent=0):
+    """Format decoded log AttributeDict as a nice str."""
+    ret_str_list = []
+    for key, value in log.items():
+        if isinstance(value, AttributeDict):
+            str_value = f'\n{format_log_str(value, 4)}'
+        elif isinstance(value, HexBytes):
+            str_value = value.hex()
+        else:
+            str_value = str(value)
+        item_str = f'{" " * indent}{key}: {str_value}'
+        if key == 'event':
+            ret_str_list.insert(0, item_str)
+        else:
+            ret_str_list.append(item_str)
+    return '\n'.join(ret_str_list)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Pull transaction information.')
+    parser = argparse.ArgumentParser(
+        description='Pull transaction information.')
     parser.add_argument('txn_hash', type=str, help='the transaction hash')
-    parser.add_argument('-p', '--abi_path', type=str, help='path to contract abi')
+    parser.add_argument('-p', '--abi_path', type=str, help='path to contract abi',
+                        default=os.path.join(
+                            os.path.dirname(__file__), '../constants/abi/beanstalk_abi.json'))
     args = parser.parse_args()
 
     web3 = Web3(WebsocketProvider(URL, websocket_timeout=60))
@@ -115,6 +144,7 @@ if __name__ == '__main__':
         # Use below line if you want decoded logs for a specific event only.
         # decoded_logs = get_decoded_logs_by_event(receipt, contract, 'AddDeposit')
 
-        print('\nDecoded logs:\n' + '\n\n'.join([pformat(dict(log)) for log in decoded_logs]))
+        print('\nDecoded logs:\n' +
+              '\n\n'.join([format_log_str(log) for log in decoded_logs]))
     else:
         print('\nNot decoding logs. Contract ABI required.')
