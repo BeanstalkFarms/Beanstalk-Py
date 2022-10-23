c8a4d589d3b4b54b62269b7265ff1e6f9d1cbe5a
funderberker <meellone01@gmail.com>
2022-10-23 15:48:24 +0800

    [tools] Add tool to search for events by contract

---
+++ tools/event_search.py
@@ -0,0 +1,79 @@
+"""
+Tool to search by recency through all transactions at address for an event and display txn hashes.
+
+Example use:
+python3 -m tools.event_search EVENT_DEFINITION
+"""
+
+import argparse
+import logging
+import os
+from web3 import Web3, WebsocketProvider
+
+
+logging.basicConfig(level=logging.WARNING)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Search for event in recent transactions.')
+    parser.add_argument('event_definition', type=str,
+                        help='the str of the event contract definition. ex)  RemoveDeposit(address,address,uint32,uint256)')
+    # parser.add_argument('-l', '--list', action='store_true', help='list beanstalk event definitions')
+    parser.add_argument('-n', '--num_txns', type=int, default=1,
+                        help='the max number of matching txns to return')
+    parser.add_argument('-a', '--address', type=str,
+                        default='0xC1E088fC1323b20BCBee9bd1B9fC9546db5624C5', help='the contract to check events at (default: Beanstalk addr)')
+    parser.add_argument('-b', '--max_block', type=int, default=12974075,
+                        help='Oldest block to check (default: inception of Beanstalk)')
+    parser.add_argument('-k', '--key', type=str,
+                        help='alchemy key (default pulls env var ALCHEMY_ETH_API_KEY)',
+                        default=os.environ['ALCHEMY_ETH_API_KEY'])
+    args = parser.parse_args()
+
+    # # List Beanstalk events, but does not include args necessary to build sig.
+    # import tools.util
+    # if args.list:
+    #     contract = tools.util.load_contract_from_abi(os.path.join(os.path.dirname(__file__), '../constants/abi/beanstalk_abi.json'))
+    #     for event in contract.events:
+    #         print(event.event_name)
+    #     exit(0)
+
+    URL = 'wss://eth-mainnet.g.alchemy.com/v2/' + args.key
+    web3 = Web3(WebsocketProvider(URL, websocket_timeout=60))
+
+    event_signature_hash = Web3.keccak(text=args.event_definition).hex()
+
+    latest_searched_block = web3.eth.get_block('latest').number
+    blocks_to_check = 1000
+    entry_matches = []
+    while len(entry_matches) < args.num_txns and latest_searched_block > args.max_block:
+        from_block = latest_searched_block - blocks_to_check
+        to_block = latest_searched_block - 1
+        filter = web3.eth.filter({
+            "address": args.address,
+            "topics": [event_signature_hash],
+            "fromBlock": from_block,
+            "toBlock": to_block
+        })
+        logging.info(
+            f'Checking blocks {from_block} to {to_block} for {args.event_definition}')
+        entries = filter.get_all_entries()
+        for entry in reversed(entries):  # Newest to oldest.
+            entry_matches.append(
+                (entry['transactionHash'].hex(), entry['blockNumber']))  # txn hash
+            if len(entry_matches) >= args.num_txns:
+                break
+
+        # Do not search blocks that have already been filtered against.
+        latest_searched_block = latest_searched_block - blocks_to_check
+        # Scale up size of search to keep things quick.
+        blocks_to_check *= 2
+
+    if len(entry_matches) == 0:
+        print(
+            f'No transactions found with matching event definition - {args.event_definition}')
+    else:
+        print(f'Transactions containing event definition:')
+        for match in entry_matches:
+            print(f'{match[0]}  (block: {match[1]})')
