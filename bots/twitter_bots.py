574477049850b05cc8b0e86c4a85c03956378853
funderberker <meellone01@gmail.com>
2022-12-05 09:02:04 +0800

    [twitter] fix root bot bug

--- bots/twitter_bots.py
+++ bots/twitter_bots.py
@@ -84,7 +84,7 @@ class RootTwitterBot(TwitterBot):
         self.token_monitor.start()
 
         self.uniswap_monitor = util.RootUniswapMonitor(
-            self.send_msg_exchange, prod=prod, dry_run=False)
+            self.send_msg, prod=prod, dry_run=False)
         self.uniswap_monitor.start()
 
     def stop(self):
