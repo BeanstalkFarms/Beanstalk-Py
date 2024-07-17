def70ddefa6a2d56e34016f2ca5d3de2298b6946
Soil King <157099073+soilking@users.noreply.github.com>
2024-07-17 10:17:27 -0700

    fix quote issue

--- bots/util.py
+++ bots/util.py
@@ -576,7 +576,7 @@ class BasinPeriodicMonitor(Monitor):
         other_wells_liquidity = 0
         for well in wells:
             if well["id"] in {token.lower() for token in {BEAN_ETH_WELL_ADDR, BEAN_WSTETH_WELL_ADDR}}:
-                whitelisted_wells_str += f"\n- 🌱 {TOKEN_SYMBOL_MAP.get(well["id"])} Liquidity: ${round_num_auto(float(well["dailySnapshots"][0]["totalLiquidityUSD"]), sig_fig_min=2, abbreviate=True)}"
+                whitelisted_wells_str += f'\n- 🌱 {TOKEN_SYMBOL_MAP.get(well["id"])} Liquidity: ${round_num_auto(float(well["dailySnapshots"][0]["totalLiquidityUSD"]), sig_fig_min=2, abbreviate=True)}'
             else:
                 other_wells_liquidity += float(well["dailySnapshots"][0]["totalLiquidityUSD"]);
             total_liquidity += float(well["dailySnapshots"][0]["totalLiquidityUSD"])
