c64d275dd82c8e630117b67402e209981b475bcc
Soil King <157099073+soilking@users.noreply.github.com>
2024-07-29 17:07:50 -0700

    Update basin sidebar

--- bots/util.py
+++ bots/util.py
@@ -2227,7 +2227,7 @@ class BasinStatusPreviewMonitor(PreviewMonitor):
     """
 
     def __init__(self, name_function, status_function):
-        super().__init__("BasinStatus", name_function, status_function, 1)
+        super().__init__("BasinStatus", name_function, status_function, 2)
         self.last_name = ""
         self.basin_graph_client = BasinSqlClient()
 
@@ -2236,36 +2236,33 @@ class BasinStatusPreviewMonitor(PreviewMonitor):
             self.wait_for_next_cycle()
             self.iterate_display_index()
 
-            bean_wsteth_liquidity = 0
-            bean_wsteth_volume = 0
-            bean_eth_liquidity = 0
-            bean_eth_volume = 0
+            liquidity = 0
+            volume = 0
+            well_count = 0
             wells = self.basin_graph_client.get_wells_stats()
 
             for well in wells:
-                if well["id"].lower() == BEAN_ETH_WELL_ADDR.lower():
-                    bean_eth_liquidity += float(well["totalLiquidityUSD"])
-                    bean_eth_volume += float(well["cumulativeTradeVolumeUSD"])
-                elif well["id"].lower() == BEAN_WSTETH_WELL_ADDR.lower():
-                    bean_wsteth_liquidity += float(well["totalLiquidityUSD"])
-                    bean_wsteth_volume += float(well["cumulativeTradeVolumeUSD"])
+                if well["id"].lower() in [BEAN_ETH_WELL_ADDR.lower(), BEAN_WSTETH_WELL_ADDR.lower()]:
+                    liquidity += float(well["totalLiquidityUSD"])
+                    volume += float(well["cumulativeTradeVolumeUSD"])
+                    well_count += 1
 
-            if bean_eth_liquidity == 0 or bean_wsteth_liquidity == 0:
+            if liquidity == 0:
                 logging.warning(
                     "Missing required Well liquidity data in subgraph query result. Skipping update..."
                 )
                 continue
 
-            # root_bdv = self.root_client.get_root_token_bdv()
-            name_str = f"Liq: ${round_num(bean_eth_liquidity + bean_wsteth_liquidity, 0)}"
+            name_str = f"Liq: ${round_num(liquidity, 0)}"
             if name_str != self.last_name:
                 self.name_function(name_str)
                 self.last_name = name_str
 
             # Rotate data and update status.
             if self.display_index == 0:
-                # self.status_function(f"Cumul Vol: ${round_num(bean_eth_volume/1000, 0)}k")
-                self.status_function(f"BEANETH")
+                self.status_function(f"Cumul Vol: ${round_num(volume/1000000, 2)}m")
+            else:
+                self.status_function(f"{well_count} Wells")
 
 
 class ParadoxPoolsPreviewMonitor(PreviewMonitor):
