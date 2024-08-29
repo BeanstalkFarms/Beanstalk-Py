a81e0ffa091102920b0b165cb4d1f3a9e39cf510
Soil King <157099073+soilking@users.noreply.github.com>
2024-08-29 15:39:27 -0700

    move to src folder

--- dev.sh
+++ dev.sh
@@ -1,5 +1,8 @@
 # Load environment variables from .env.dev file
-export $(grep -v '^#' .env.dev | xargs)
+export $(grep -v '^#' .env | xargs)
+
+# Set path to the src folder
+export PYTHONPATH=$PYTHONPATH:$(pwd)/src
 
 # Configure dry run (test transactions)
 # Provide comma separated hashes, or "all" to run through the whole collection
